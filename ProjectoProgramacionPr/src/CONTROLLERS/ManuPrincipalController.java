4311d648824ac687406c1212b283ddeb82e0123a
Jorge Anaya <jorgeanaya94@hotmail.com>
2022-11-05 13:35:25 -0500

    Agregado City

--- ProjectoProgramacionPr/src/CONTROLLERS/ManuPrincipalController.java
+++ ProjectoProgramacionPr/src/CONTROLLERS/ManuPrincipalController.java
@@ -3,34 +3,177 @@ package CONTROLLERS;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import MODELO.City;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableRow;
 
 public class ManuPrincipalController {
+
+    @FXML
+    private TextField Capital_Country;
+
+    @FXML
+    private TextField Code2_Country;
+
+    @FXML
+    private TextField Code_City;
+
+    @FXML
+    private TextField Code_Country;
+
+    @FXML
+    private TextField Code_Country1;
+
+    @FXML
+    private TextField Continent_Country;
+
+    @FXML
+    private TextField CountryCode_CL;
+
+    @FXML
+    private Button CreateCountry;
+
+    @FXML
+    private Button CreateCountry1;
+
+    @FXML
+    private Button Create_CL;
+
+    @FXML
+    private Button Create_City;
+
+    @FXML
+    private Button DeleteCountry;
+
+    @FXML
+    private Button DeleteCountry1;
+
+    @FXML
+    private Button Delete_CL;
+
+    @FXML
+    private TextField District_City;
+
+    @FXML
+    private TextField GNP_Country;
+
+    @FXML
+    private TextField GNP_Old_Country;
+
+    @FXML
+    private TextField Gov_Country;
+
+    @FXML
+    private TextField Head_Country;
+
+    @FXML
+    private TextField ID_City;
+
+    @FXML
+    private TextField Language_CL;
+
+    @FXML
+    private TextField Life_Country;
+
+    @FXML
+    private TextField Local_Country;
+
+    @FXML
+    private MenuItem MoveCL;
+
+    @FXML
+    private MenuItem MoveCity;
+
+    @FXML
+    private MenuItem MoveCountry;
+
+    @FXML
+    private MenuItem MoveOperaciones;
+
+    @FXML
+    private TextField Name_City;
+
+    @FXML
+    private TextField Name_Country;
+
+    @FXML
+    private TextField Official_CL;
+
+    @FXML
+    private TextField Perc_CL;
+
+    @FXML
+    private TextField Pop_City;
+
+    @FXML
+    private TextField Pop_Country;
+
+    @FXML
+    private TextField Region_Country;
+
+    @FXML
+    private TextField SA_Country;
 
     @FXML
     private MenuItem Salir;
 
     @FXML
-    private ImageView carrerasImg;
+    private Button UpdateCountry;
+
+    @FXML
+    private Button UpdateCountry1;
+
+    @FXML
+    private Button Update_CL;
+
+    @FXML
+    private Button Update_City;
+
+    @FXML
+    private TableColumn<City, String> code_col;
+
+    @FXML
+    private TableColumn<City, String> district_col;
+
+    @FXML
+    private Label doM;
+
+    @FXML
+    private Pane doMenu;
 
     @FXML
     private Button eliminarPrograma;
 
     @FXML
-    private Pane gestionPrograma;
+    private Pane gestionCL;
 
     @FXML
-    private ImageView imagePrincipal;
+    private Pane gestionCity;
+
+    @FXML
+    private Pane gestionCountry;
+
+    @FXML
+    private Pane gestionOperaciones;
+
+    @FXML
+    private TableColumn<City, String> id_col;
 
     @FXML
     private MenuBar menuBar;
@@ -40,251 +183,256 @@ public class ManuPrincipalController {
 
     @FXML
     private MenuItem menuPrincipal;
-    @FXML
-    private Button Create_City;
-    @FXML
-    private Button Update_City;
-    @FXML
-    private TextField ID_City;
-    @FXML
-    private TextField Name_City;
-    @FXML
-    private TextField Code_City;
-    @FXML
-    private TextField District_City;
-    @FXML
-    private TextField Pop_City;
-    @FXML
-    private Pane gestionPrograma1;
-    @FXML
-    private ImageView carrerasImg1;
-    @FXML
-    private TextField Code_Country;
-    @FXML
-    private TextField Name_Country;
-    @FXML
-    private TextField Continent_Country;
-    @FXML
-    private TextField Region_Country;
-    @FXML
-    private TextField SA_Country;
-    @FXML
-    private TextField Code2_Country;
-    @FXML
-    private TextField Pop_Country;
-    @FXML
-    private TextField Life_Country;
-    @FXML
-    private TextField GNP_Country;
-    @FXML
-    private TextField GNP_Old_Country;
-    @FXML
-    private TextField Local_Country;
-    @FXML
-    private TextField Gov_Country;
-    @FXML
-    private TextField Capital_Country;
-    @FXML
-    private TextField Head_Country;
-    @FXML
-    private Button CreateCountry;
-    @FXML
-    private Button UpdateCountry;
-    @FXML
-    private Button DeleteCountry;
-    @FXML
-    private Pane gestionPrograma2;
-    @FXML
-    private Button Create_CL;
-    @FXML
-    private Button Update_CL;
-    @FXML
-    private Button Delete_CL;
-    @FXML
-    private TextField CountryCode_CL;
-    @FXML
-    private TextField Language_CL;
-    @FXML
-    private TextField Official_CL;
-    @FXML
-    private TextField Perc_CL;
-    @FXML
-    private Pane gestionPrograma11;
-    @FXML
-    private TextField Code_Country1;
-    @FXML
-    private Button CreateCountry1;
-    @FXML
-    private Button UpdateCountry1;
-    @FXML
-    private Button DeleteCountry1;
-    @FXML
-    private MenuItem MoveCity;
-    @FXML
-    private MenuItem MoveCountry;
-    @FXML
-    private MenuItem MoveCL;
-    @FXML
-    private MenuItem MoveOperaciones;
 
-    @Deprecated
-    void buscarFoto(ActionEvent event) {
+    @FXML
+    private TableColumn<City, String> name_col;
+
+    @FXML
+    private TableColumn<City, String> pop_col;
+
+    @FXML
+    private TableView<City> table_city;
+
+    Connection con;
+    PreparedStatement pst;
+    int myIndex;
+    int id;
+
+    public void Connect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/World", "root", "");
+        } catch (ClassNotFoundException ex) {
+
+        } catch (SQLException ex) {
+
+        }
+    }
+
+    @FXML
+    void doCL(ActionEvent event) {
+        this.gestionCL.setVisible(true);
+        this.gestionCity.setVisible(false);
+        this.gestionCountry.setVisible(false);
+        this.gestionCL.toFront();
+    }
+
+    @FXML
+    void doCity(ActionEvent event) {
+        this.gestionCL.setVisible(false);
+        this.gestionCity.setVisible(true);
+        this.gestionCountry.setVisible(false);
+        this.gestionCity.toFront();
+    }
+
+    @FXML
+    void doCountry(ActionEvent event) {
+        this.gestionCL.setVisible(false);
+        this.gestionCity.setVisible(false);
+        this.gestionCountry.setVisible(true);
+        this.gestionCountry.toFront();
+    }
+
+    @FXML
+    void doCreateCity(ActionEvent event) {
+        String id_city, name_city, code_city, district_city, pop_city;
+        id_city = ID_City.getText();
+        name_city = Name_City.getText();
+        code_city = Code_City.getText();
+        district_city = District_City.getText();
+        pop_city = Pop_City.getText();
+
+        try {
+            pst = con.prepareStatement("insert into city(ID,Name,CountryCode,District,Population) values(?,?,?,?,?)");
+            pst.setString(1, id_city);
+            pst.setString(2, name_city);
+            pst.setString(3, code_city);
+            pst.setString(4, district_city);
+            pst.setString(5, pop_city);
+            pst.executeUpdate();
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("City Registation");
+
+            alert.setHeaderText("City Registation");
+            alert.setContentText("Record Added!");
+
+            alert.showAndWait();
+
+            table_city();
+
+            ID_City.setText("");
+            Name_City.setText("");
+            Code_City.setText("");
+            District_City.setText("");
+            Pop_City.setText("");
+            ID_City.requestFocus();
+        } catch (SQLException ex) {
+            Logger.getLogger(ManuPrincipalController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void table_city() {
+        Connect();
+        ObservableList<City> city = FXCollections.observableArrayList();
+        try {
+            pst = con.prepareStatement("select ID,Name,CountryCode,District,Population from city");
+            ResultSet rs = pst.executeQuery();
+            {
+                while (rs.next()) {
+                    City st = new City();
+                    st.setId(rs.getString("ID"));
+                    st.setName(rs.getString("Name"));
+                    st.setDistrict(rs.getString("District"));
+                    st.setCountry_code(rs.getString("Country Code"));
+                    st.setPopulation(rs.getString("Population"));
+                    city.add(st);
+                }
+            }
+
+            table_city.setItems(city);
+            id_col.setCellValueFactory(f -> f.getValue().idProperty());
+            name_col.setCellValueFactory(f -> f.getValue().namProperty());
+            district_col.setCellValueFactory(f -> f.getValue().dStringProperty());
+            code_col.setCellValueFactory(f -> f.getValue().codeProperty());
+            pop_col.setCellValueFactory(f -> f.getValue().pStringProperty());
+
+        }
+
+        catch (SQLException ex) {
+            Logger.getLogger(ManuPrincipalController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        table_city.setRowFactory(tv -> {
+            TableRow<City> myRow = new TableRow<>();
+            myRow.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 1 && (!myRow.isEmpty())) {
+                    myIndex = table_city.getSelectionModel().getSelectedIndex();
+                    id = Integer.parseInt(String.valueOf(table_city.getItems().get(myIndex).getId()));
+                    ID_City.setText(table_city.getItems().get(myIndex).getId());
+                    Name_City.setText(table_city.getItems().get(myIndex).getName());
+                    Code_City.setText(table_city.getItems().get(myIndex).getCountry_code());
+                    Pop_City.setText(table_city.getItems().get(myIndex).getPopulation());
+                    district_col.setText(table_city.getItems().get(myIndex).getDistrict());
+
+                }
+            });
+            return myRow;
+        });
 
     }
 
-    @Deprecated
-    void displaySelectedProgram(MouseEvent event) {
+    @FXML
+    void doCreateCountry(ActionEvent event) {
 
     }
 
-    @Deprecated
-    void doAction2(ActionEvent event) {
+    @FXML
+    void doCreate_CL(ActionEvent event) {
 
     }
 
-    @Deprecated
-    void doAction4(ActionEvent event) {
+    @FXML
+    void doDeleteCity(ActionEvent event) {
+        myIndex = table_city.getSelectionModel().getSelectedIndex();
+        id = Integer.parseInt(String.valueOf(table_city.getItems().get(myIndex).getId()));
+
+        try {
+            pst = con.prepareStatement("delete from city where id = ? ");
+            pst.setInt(1, id);
+            pst.executeUpdate();
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("City Registation");
+
+            alert.setHeaderText("City Registation");
+            alert.setContentText("Deleted!");
+
+            alert.showAndWait();
+            table_city();
+        } catch (SQLException ex) {
+            Logger.getLogger(ManuPrincipalController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    void doDeleteCountry(ActionEvent event) {
 
     }
 
-    @Deprecated
-    void doBackEst(ActionEvent event) {
+    @FXML
+    void doDelete_CL(ActionEvent event) {
 
     }
 
-    @Deprecated
-    void doBuscarEst(ActionEvent event) {
-
+    @FXML
+    void doMenu(ActionEvent event) {
+        this.gestionCL.setVisible(false);
+        this.gestionCity.setVisible(false);
+        this.gestionCountry.setVisible(false);
+        this.gestionOperaciones.setVisible(false);
     }
 
-    @Deprecated
-    void doBuscarPrograma(ActionEvent event) {
-
-    }
-
-    @Deprecated
-    void doCargarTodos(ActionEvent event) {
-
-    }
-
-    @Deprecated
-    void doEliminarEst(ActionEvent event) {
-
-    }
-
-    @Deprecated
-    void doEliminarPrograma(ActionEvent event) {
-
-    }
-
-    @Deprecated
-    void doGuardarEst(ActionEvent event) {
-
-    }
-
-    @Deprecated
-    void doGuardarPrograma(ActionEvent event) {
-
-    }
-
-    @Deprecated
-    void doModificarEst(ActionEvent event) {
-
-    }
-
-    @Deprecated
-    void doModificarPrograma(ActionEvent event) {
-
-    }
-
-    @Deprecated
-    void doNextEst(ActionEvent event) {
-
-    }
-
-    @Deprecated
-    void doNuevoEst(ActionEvent event) {
-
-    }
-
-    @Deprecated
-    void doNuevoPrograma(ActionEvent event) {
-
-    }
-
-    @Deprecated
-    void doOpcion1(ActionEvent event) {
-
-    }
-
-    @Deprecated
-    void doPeriodo(ActionEvent event) {
-
+    @FXML
+    void doOperations(ActionEvent event) {
+        this.gestionCL.setVisible(false);
+        this.gestionCity.setVisible(false);
+        this.gestionCountry.setVisible(false);
+        this.gestionOperaciones.setVisible(true);
+        this.gestionOperaciones.toFront();
     }
 
     @FXML
     void doSalir(ActionEvent event) {
-
+        var stage = (Stage) this.gestionCountry.getScene().getWindow();
+        stage.close();
+        System.exit(0);
     }
 
-    @Deprecated
-    void doYear(ActionEvent event) {
+    @FXML
+    void doUpdateCity(ActionEvent event) {
+
+        myIndex = table_city.getSelectionModel().getSelectedIndex();
+        id = Integer.parseInt(String.valueOf(table_city.getItems().get(myIndex).getId()));
+
+        String id_city, name_city, code_city, district_city, pop_city;
+
+        id_city = ID_City.getText();
+        name_city = Name_City.getText();
+        code_city = Code_City.getText();
+        district_city = District_City.getText();
+        pop_city = Pop_City.getText();
+
+        try {
+            pst = con.prepareStatement(
+                    "update City set Name = ?,CountryCode = ? ,District = ?, Population = ? where ID = ? ");
+            pst.setString(1, name_city);
+            pst.setString(2, code_city);
+            pst.setString(3, district_city);
+            pst.setString(4, pop_city);
+            pst.setString(5, id_city);
+            pst.executeUpdate();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("City Registation");
+
+            alert.setHeaderText("City Registation");
+            alert.setContentText("Updated!");
+
+            alert.showAndWait();
+            table_city();
+        } catch (SQLException ex) {
+            Logger.getLogger(ManuPrincipalController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    void doUpdateCountry(ActionEvent event) {
 
     }
 
     @FXML
-    public void doCreateCity(ActionEvent actionEvent) {
+    void doUpdate_CL(ActionEvent event) {
+
     }
 
-    @FXML
-    public void doUpdateCity(ActionEvent actionEvent) {
-    }
-
-    @FXML
-    public void doDeleteCity(ActionEvent actionEvent) {
-    }
-
-    @FXML
-    public void doCreate_CL(ActionEvent actionEvent) {
-    }
-
-    @FXML
-    public void doUpdate_CL(ActionEvent actionEvent) {
-    }
-
-    @FXML
-    public void doDelete_CL(ActionEvent actionEvent) {
-    }
-
-    @FXML
-    public void doCreateCountry(ActionEvent actionEvent) {
-    }
-
-    @FXML
-    public void doUpdateCountry(ActionEvent actionEvent) {
-    }
-
-    @FXML
-    public void doDeleteCountry(ActionEvent actionEvent) {
-    }
-
-    @FXML
-    public void doMenu(ActionEvent actionEvent) {
-    }
-
-    @FXML
-    public void doCity(ActionEvent actionEvent) {
-    }
-
-    @FXML
-    public void doCountry(ActionEvent actionEvent) {
-    }
-
-    @FXML
-    public void doCL(ActionEvent actionEvent) {
-    }
-
-    @FXML
-    public void doOperations(ActionEvent actionEvent) {
-    }
 }
--- ProjectoProgramacionPr/src/MODELO/City.java
+++ ProjectoProgramacionPr/src/MODELO/City.java
@@ -1,62 +1,101 @@
 package MODELO;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class City {
-    private String name, country_code, district;
-    private int id, population;
+    // private String name, country_code, district;
+    // private int id, population;
+
+    private final StringProperty name;
+    private final StringProperty country_code;
+    private final StringProperty district;
+    private final SimpleStringProperty id;
+    private final SimpleStringProperty population;
 
     public City() {
-        this.name = "";
-        this.country_code = "";
-        this.district = "";
-        this.id = 0;
-        this.population = 0;
+
+        id = new SimpleStringProperty(this, "id");
+        name = new SimpleStringProperty(this, "name");
+        country_code = new SimpleStringProperty(this, "country_code");
+        district = new SimpleStringProperty(this, "district");
+        population = new SimpleStringProperty(this, "population");
     }
 
-    public City(String name, String country_code, String district, int id, int population) {
-        this.name = name;
-        this.country_code = country_code;
-        this.district = district;
-        this.id = id;
-        this.population = population;
-    }
+    // this.name = "";
+    // this.country_code = "";
+    // this.district = "";
+    // this.id = 0;
+    // this.population = 0;
+    // }
+
+    // public City(String name, String country_code, String district, int id, int
+    // population) {
+    // this.name = name;
+    // this.country_code = country_code;
+    // this.district = district;
+    // this.id = id;
+    // this.population = population;
+    // }
 
     public String getName() {
-        return name;
+        return name.get();
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String newname) {
+        name.set(newname);
     }
 
     public String getCountry_code() {
-        return country_code;
+        return country_code.get();
     }
 
-    public void setCountry_code(String country_code) {
-        this.country_code = country_code;
+    public void setCountry_code(String newcountry_code) {
+        country_code.set(newcountry_code);
     }
 
     public String getDistrict() {
-        return district;
+        return district.get();
     }
 
-    public void setDistrict(String district) {
-        this.district = district;
+    public void setDistrict(String newdistrict) {
+        district.set(newdistrict);
     }
 
-    public int getId() {
+    public String getId() {
+        return id.get();
+    }
+
+    public void setId(String newid) {
+        id.set(newid);
+    }
+
+    public String getPopulation() {
+        return population.get();
+    }
+
+    public void setPopulation(String newpopulation) {
+        population.set(newpopulation);
+        ;
+    }
+
+    public StringProperty idProperty() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public StringProperty namProperty() {
+        return name;
     }
 
-    public int getPopulation() {
+    public StringProperty dStringProperty() {
+        return district;
+    }
+
+    public StringProperty codeProperty() {
+        return country_code;
+    }
+
+    public StringProperty pStringProperty() {
         return population;
-    }
-
-    public void setPopulation(int population) {
-        this.population = population;
     }
 }
